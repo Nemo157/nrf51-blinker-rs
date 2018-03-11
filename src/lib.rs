b295638dd19a9e4828b67c0cd543c87e818bef50
Wim Looman <wim@nemo157.com>
2018-03-11 14:27:29 +0100

    futures-io test

--- src/lib.rs
+++ src/lib.rs
@@ -5,69 +5,28 @@
 #![feature(proc_macro)]
 
 extern crate futures_await as futures;
-extern crate futures_hal as hal;
-
-use core::time::Duration;
 
 use futures::prelude::{async, await};
-use futures::future::{self, Either};
-use futures::{FutureExt, StreamExt};
-use hal::{Cancellable, Periodic, DetectingInputPin, OutputPin, Event};
+use futures::io::{CoreAsyncRead, CoreAsyncReadExt, CoreAsyncWrite, CoreAsyncWriteExt};
+
+#[derive(Debug)]
+pub enum Error<TxError, RxError> {
+    Tx(TxError),
+    Rx(RxError),
+}
 
 #[async]
 #[allow(unreachable_code, unreachable_patterns)]
-pub fn main<'a, Timer, Led, Button>(
-    mut timer: Timer,
-    mut led: Led,
-    button: Button) -> Result<!, !>
+pub fn main<'a, Tx, Rx>(
+    mut tx: Tx,
+    mut rx: Rx) -> Result<!, Error<Tx::Error, Rx::Error>>
 where
-    Timer: Periodic + 'a,
-    Led: OutputPin + 'a,
-    Button: DetectingInputPin + 'a,
+    Tx: CoreAsyncWrite + 'a,
+    Rx: CoreAsyncRead + 'a,
 {
-    let timeout = Duration::from_secs(3);
-    let debounce = Duration::from_millis(200);
-
-    let mut detector = button.detect(Event::HighToLow).into_future();
-
     loop {
-        // If button is pushed, or timeout has passed
-        match await!(timer.start(timeout).select(detector)) {
-            Ok(Either::Left((timer_, detector_))) => {
-                // Toggle LED
-                if led.is_high() {
-                    led.set_low();
-                } else {
-                    led.set_high();
-                }
-
-                // Timer was triggered, no additional action needed
-                timer = timer_;
-                detector = detector_;
-            },
-            Ok(Either::Right(((_, detector_), timer_))) => {
-                // Toggle LED
-                if led.is_high() {
-                    led.set_low();
-                } else {
-                    led.set_high();
-                }
-
-                // Button was triggered, debounce the button by ignoring events
-                // for debounce period.
-                let debounce_timer = timer_.cancel().start(debounce);
-                let ignored_detector = detector_.skip_while(|_| future::ok(true)).into_future();
-                match await!(debounce_timer.select(ignored_detector)) {
-                    Ok(Either::Left((timer_, detector_))) => {
-                        timer = timer_;
-                        // Extract the actual detector from the `StreamFuture<SkipWhile<D>>`
-                        detector = detector_.into_inner().unwrap().into_inner().into_future();
-                    }
-                    Ok(Either::Right(_)) => {
-                        unreachable!("Ignored button should never return");
-                    }
-                }
-            },
-        };
+        let (_, bytes) = await!((&mut rx).read_exact([0; 5])).map_err(Error::Rx)?;
+        let (tx_, _) = await!(tx.write_all(bytes)).map_err(Error::Tx)?;
+        tx = tx_;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #![no_std]
 
+#![feature(never_type)]
+
 extern crate wfe_executor;
 extern crate nrf51;
 extern crate nrf51_hal;
+extern crate nb;
+extern crate embedded_hal;
 
 extern crate nrf51_blinker;
 
 use wfe_executor::Executor;
-use nrf51_hal::{timer::Timer, gpio::GpioExt};
+use nrf51_hal::{gpio::GpioExt, serial::{Serial, BAUDRATEW}};
+use nb::io::{Read, Write};
+use embedded_hal::serial::io::{reader, writer};
 
 pub fn main() {
     let core = nrf51::CorePeripherals::take().unwrap();
@@ -15,11 +21,18 @@ pub fn main() {
 
     let gpio = peripherals.GPIO.split();
 
-    let timer = Timer::new(peripherals.TIMER0);
-    let led = gpio.pin21.into_open_drain_output();
-    let button = gpio.pin15.into_pull_up_input();
+    let uart = Serial::uart0(
+        peripherals.UART0,
+        gpio.pin9.into_push_pull_output().downgrade(),
+        gpio.pin11.into_floating_input().downgrade(),
+        BAUDRATEW::BAUD1M);
 
-    let future = nrf51_blinker::main(timer, led, button);
+    let (tx, rx) = uart.split();
+    let future = nrf51_blinker::main(writer(tx).as_async(), reader(rx).as_async());
 
-    Executor::new(core).run_stable(future);
+    match Executor::new(core).run_stable(future) {
+        Err(err) => {
+            panic!("{:?}", err);
+        }
+    }
 }
